7cf48f07bf33f106daf5db4221e72ba5e3eb6a92
zertidana <94858570+zertidana@users.noreply.github.com>
2023-11-14 14:51:17 +0000

    kjb

---
+++ api/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.shortcuts import render
+
+
+def main_spa(request: HttpRequest) -> HttpResponse:
+    return render(request, 'api/spa/index.html', {})
