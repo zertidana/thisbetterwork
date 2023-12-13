a5e7afa2386436f9725df05ebaafaf0028c62086
zertidana <94858570+zertidana@users.noreply.github.com>
2023-12-13 13:02:08 +0000

    Backend for news articles is done

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, News
 
-# Register your models here.
+admin.site.register(Category)
+
+class AdminNews(admin.ModelAdmin):
+    list_display = ('title', 'category', 'add_time')
+
+admin.site.register(News)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    category_image=models.ImageField(upload_to='imgs/')
+
+    def __str__(self):
+        """Return the title of the category."""
+        return self.title
+        
+    class Meta:
+        verbose_name_plural='Categories'
+
+class News(models.Model):
+    """ 
+    A Category has many news articles within it, so the 
+    foreign key below describes a many to one relationship
+    Also that, once the category is deleted, all related artciles are also deleted
+    """
+    category=models.ForeignKey(Category,on_delete=models.CASCADE) 
+    title=models.CharField(max_length=300)
+    image=models.ImageField(upload_to='imgs/')
+    detail=models.TextField()
+    add_time=models.TimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural='News'
+
+    def __str__(self):
+        return self.title
