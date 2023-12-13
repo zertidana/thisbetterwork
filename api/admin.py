b8faff211448c6725ebccb8d094b4dd889e09d12
zertidana <94858570+zertidana@users.noreply.github.com>
2023-12-13 13:16:16 +0000

    Backend commenting done

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Category, News
+from .models import Category, News, Comment
 
 admin.site.register(Category)
 
 class AdminNews(admin.ModelAdmin):
+    # It isn't showing the time for some reason; fix it 
     list_display = ('title', 'category', 'add_time')
 
 admin.site.register(News)
+
+class AdminComment(admin.ModelAdmin):
+    list_display = ('news', 'email', 'comment', 'status')
+
+admin.site.register(Comment, AdminComment)
+
+
--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from turtle import title
 from django.db import models
 
+# Categories
 class Category(models.Model):
     title = models.CharField(max_length=200)
     category_image=models.ImageField(upload_to='imgs/')
@@ -8,10 +9,11 @@ class Category(models.Model):
     def __str__(self):
         """Return the title of the category."""
         return self.title
-        
+
     class Meta:
         verbose_name_plural='Categories'
 
+# News Articles
 class News(models.Model):
     """ 
     A Category has many news articles within it, so the 
@@ -29,3 +31,16 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+# Comments
+class Comment(models.Model):
+    news=models.ForeignKey(News,on_delete=models.CASCADE) 
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=200)
+    image=models.ImageField(upload_to='imgs/')
+    comment=models.TextField()
+    add_time=models.TimeField(auto_now_add=True)
+    status=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comment
