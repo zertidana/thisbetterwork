a879f1f64c011493bae68bb5737ce9bccfecafbf
zertidana <94858570+zertidana@users.noreply.github.com>
2023-12-13 20:49:04 +0000

    Merge branch 'main' of https://github.com/zertidana/thisbetterwork

--- api/urls.py
+++ api/urls.py
@@ -18,12 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
 from . import views
-from .views import SportsArticlesView
 
 from .views import main_spa
 
 urlpatterns = [
     path('', views.home,name='home'),
-    path('articles/sports/', SportsArticlesView.as_view(), name='sports-articles'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,5 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Article
-from .serializers import ArticleSerializer
-
 
 def main_spa(request: HttpRequest) -> HttpResponse:
     return render(request, 'api/spa/index.html', {})
@@ -12,8 +7,3 @@ def main_spa(request: HttpRequest) -> HttpResponse:
 def home(request):
     return render(request,'App.vue')
 
-class SportsNewsView(APIView):
-    def get(self, request):
-        sports_articles = News.objects.filter(category__name='Sports')
-        serializer = NewsSerializer(sports_articles, many=True)
-        return Response(serializer.data)
