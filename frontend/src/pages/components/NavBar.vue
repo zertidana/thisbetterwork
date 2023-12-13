1e8ca787ff455ab8c6da039ce738cc28d3bbdf04
Visarddd <ec21715@qmul.ac.uk>
2023-12-13 17:29:02 +0000

    navbar completed

--- frontend/src/pages/components/NavBar.vue
+++ frontend/src/pages/components/NavBar.vue
@@ -1,108 +1,37 @@
 <template>
-  <nav class="navbar">
-    <ul>
-      <li><router-link to="/App.vue">Home</router-link></li>
-
-            
-            <li class="dropdown"><router-link
-                class="dropbtn"
-                :to="{name: 'All Categories'}"
-            >
-                All Categories
-            </router-link>
-                <div class="dropdown-content">
-                    <router-link to="/SportsPage.vue">Sports</router-link>
-                    <router-link to="/FashionPage.vue">Fashion</router-link>
-                    <router-link to="/EducationPage.vue">Education</router-link>
-                    <router-link to="/ArtPage.vue">Art</router-link>
-                    <router-link to="/WorldPage.vue">World News</router-link>
-                    <router-link to="/FinancePage.vue">Finance</router-link>
-                </div></li>
-
-            <li style="float:right"><a class="active" href="http://127.0.0.1:8000/accounts/login/"> Login</a></li>
-            <li style="float:right"><a class="active" href="http://127.0.0.1:8000/accounts/signup/"> Sign Up</a></li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/App.vue">Home</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              All Categories
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><router-link class="dropdown-item" to="/SportsPage.vue">Sports</router-link></li>
+              <li><router-link class="dropdown-item" to="/FashionPage.vue">Fashion</router-link></li>
+              <li><router-link class="dropdown-item" to="/EducationPage.vue">Education</router-link></li>
+              <li><router-link class="dropdown-item" to="/ArtPage.vue">Art</router-link></li>
+              <li><router-link class="dropdown-item" to="/WorldPage.vue">World News</router-link></li>
+              <li><router-link class="dropdown-item" to="/FinancePage.vue">Finance</router-link></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link active" href="http://127.0.0.1:8000/accounts/login/">Login</a></li>
+          <li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/accounts/signup/">Sign Up</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script lang="ts">
-
+// Script content remains the same
 </script>
 
-<style scoped>
-
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-  position: sticky;
-  top: 0;
-}
-
-li {
-  display: inline;
-  float: left;
-  border-right: 1px solid #bbb;
-}
-/*
-a {
-  display: block;
-  padding: 8px;
-  background-color: #dddddd;
-}
-*/
-li a:hover, .dropdown:hover .dropbtn  {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  background-color: #111;
-}
-
-.active {
-  background-color: #0439aa;
-}
-
-li a, .dropbtn {
-  display: inline-block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li.dropdown {
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {background-color: #f1f1f1;}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-
-
-
-</style>
-
+<!-- Since Bootstrap is being used, you might not need additional custom styles -->
