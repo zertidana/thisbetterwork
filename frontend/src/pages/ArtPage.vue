f444ae8ed9118e99ec8aedc6082863e8ca5589de
Visarddd <v.neza@se21.qmul.ac.uk>
2023-12-15 15:36:23 +0000

    fixed

--- frontend/src/pages/ArtPage.vue
+++ frontend/src/pages/ArtPage.vue
@@ -1,24 +1,106 @@
 <template>
-  <div class="h3">
-    {{ title }}
+  <div class="container sports-page">
+    <h3 class="mb-4">{{ title }}</h3>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="card">
+          <img >
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+  
 <script lang="ts">
-  import { defineComponent } from "vue";
-  import { useUserStore } from "../../stores/auth";
-  export default defineComponent({
-      data() {
-          return {
-              title: "Art Page",
-          }
-      },
-      setup(){
-        const userStore = useUserStore();
-        return { userStore };
-      }
-  })
+import { defineComponent, onMounted } from 'vue';
+import { useArticleStore } from '../../stores/articleStore';
+
+export default defineComponent({
+  setup() {
+    const articleStore = useArticleStore();
+
+    onMounted(() => {
+      articleStore.fetchArticlesByCategoryName('Sports');
+    });
+
+    return { articleStore };
+  },
+  data() {
+    return {
+      title: "Art News",
+    };
+  },
+  methods: {
+    
+  }
+});
 </script>
 
-<style scoped>
-</style>
+  
+  <style scoped>
+  .sports-page {
+      width: 80%;
+      margin: auto;
+      padding: 20px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .article {
+      margin-top: 20px;
+      border: 1px solid #ddd;
+      padding: 20px;
+  }
+  
+  .article img {
+      max-width: 100%;
+      height: auto;
+      margin-bottom: 20px;
+  }
+  
+  .article img {
+    width: 800px;
+    height: 450px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+
+  .article button {
+      background-color: #007bff;
+      color: white;
+      border: none;
+      padding: 10px 15px;
+      cursor: pointer;
+      border-radius: 5px;
+      margin-bottom: 20px;
+  }
+  
+  .article button:hover {
+      background-color: #0056b3;
+  }
+  
+  .comment-section textarea {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 10px;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+  }
+  
+  .comment-section button {
+      background-color: #28a745;
+      color: white;
+      border: none;
+      padding: 10px 15px;
+      cursor: pointer;
+      border-radius: 5px;
+  }
+  
+  .comment-section button:hover {
+      background-color: #218838;
+  }
+  </style>
